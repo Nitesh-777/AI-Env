a6e5786d5e0bff350caa03f199953c747813c455
Nitesh-777 <Nitesh.Karavadra@outlook.com>
2024-02-21 18:10:57 +0000

    new get_pose_quat method added within transform_pose that gets the relative postion of the learning agent using quartenions as provided from mujoco. Adapted from orginal get_pose method within transform_pose which is still kept to verify results from get_pose_quat are correct.

--- transform_pose.py
+++ transform_pose.py
@@ -4,33 +4,79 @@ import pytransform3d.transformations as pt
 import matplotlib.pyplot as plt
 
 
-def get_pose(pose1, pose2):
+def get_pose(agent_pose, other_pose):
+
+    def transform(x, y, angle):
+        rotation_matrix_2d = pr.active_matrix_from_angle(2, angle)
+        print(rotation_matrix_2d)
+        transformation_matrix = np.eye(4)
+        transformation_matrix[:3, :3] = rotation_matrix_2d
+        transformation_matrix[0, 3] = x
+        transformation_matrix[1, 3] = y
+
+        return transformation_matrix
+
+    agent_transform = transform(*agent_pose)
+    # print(f"pose1 transformation matrix \n{agent_transform}")
+    other_transform = transform(*other_pose)
+    # print(f"pose2 transformation matrix \n{other_transform}")
+    other_inverse = pt.invert_transform(other_transform)
+    # print(f"pose2 invert matrix \n{other_inverse}")
+
+    relative_matrix = np.dot(other_inverse, agent_transform)
+    # print(f"relative pose matrix \n{relative_matrix}")
+    final_pose = [relative_matrix[0, 3], relative_matrix[1, 3],
+                  np.arctan2(relative_matrix[1, 0], relative_matrix[0, 0])]
+    # print(f"final pose \n{final_pose}")
+    return final_pose
+
+pose = get_pose([-1, 0, np.pi/4], [1, 0, np.pi * 0.75])
+print(pose)
+
+
+def get_pose_qpos(agent_qpos, other_qpos):
+    def quat_angle(quat):
+        q_w, q_x, q_y, q_z = quat
+        angle = np.arctan2(2 * (q_w * q_z + q_x * q_y), 1 - 2 * (q_y ** 2 + q_z ** 2))
+        return angle
+    
     def transform(x, y, angle):
         rotation_matrix_2d = pr.active_matrix_from_angle(2, angle)
         transformation_matrix = np.eye(4)
         transformation_matrix[0:3, 0:3] = rotation_matrix_2d
         transformation_matrix[0, 3] = x
         transformation_matrix[1, 3] = y
-        # translation_matrix = pt.translate_transform(np.eye(4), [x, y, 0])
-        # print(translation_matrix)
-        # # rotation_matrix = pt.rotate_transform(np.eye(4), angle, [0, 0, 1])
-        # rotation_matrix = pr.matrix_from_axis_angle([0, 0, 1, angle])
-        # print(rotation_matrix)
-        # print(pr.check_matrix(rotation_matrix))
-        # transformation_matrix = np.dot(translation_matrix, rotation_matrix)
+
+
         return transformation_matrix
-    pose1_transform = transform(*agent_pose)
-    print(f"pose1 transformation matrix \n{pose1_transform}")
-    pose2_transform = transform(*helper_pose)
-    print(f"pose2 transformation matrix \n{pose2_transform}")
-    pose2_inverse = pt.invert_transform(pose2_transform)
-    print(f"pose2 inverse matrix \n{pose2_inverse}")
-    relative_matrix = np.dot(pose2_inverse, pose1_transform)
-    print(f"relative pose matrix \n{relative_matrix}")
+
+    agent_angle = quat_angle(agent_qpos[3:])
+    other_angle = quat_angle(other_qpos[3:])
+    # print(f'Agent angle ===========: {agent_angle * 180 / np.pi}')
+    # print(f'Other angle ===========: {other_angle * 180 / np.pi}')
+
+    agent_transform = transform(agent_qpos[0], agent_qpos[1], agent_angle)
+    # print(f"pose1 transformation matrix \n{agent_transform}")
+    other_transform = transform(other_qpos[0], other_qpos[1], other_angle)
+    # print(f"pose2 transformation matrix \n{other_transform}")
+
+    other_inverse = pt.invert_transform(other_transform)
+    # print(f"pose2 inverse matrix \n{other_inverse}")
+    relative_matrix = np.dot(other_inverse, agent_transform)
+    # print(f"relative pose matrix \n{relative_matrix}")
     final_pose = [relative_matrix[0, 3], relative_matrix[1, 3],
                   np.arctan2(relative_matrix[1, 0], relative_matrix[0, 0])]
-    print(f"final pose \n{final_pose}")
+    # print(f"final pose \n{final_pose}")
     return final_pose
+
+learner_qpos = [-1.00000000e+00,  0.00000000e+00,  6.99333876e-02,  9.23879533e-01,
+  6.31031096e-05, -1.52344383e-04,  3.82683432e-01]
+
+other_qpos = [ 1.00000000e+00, 0.00000000e+00,  6.99297389e-02,  3.82683432e-01,
+  5.42397542e-04,-2.24668418e-04,  9.23879533e-01]
+
+pose = get_pose_qpos(learner_qpos, other_qpos)
+print(pose)
 
 
 def visualise(ax, pose, color, label):
@@ -42,21 +88,25 @@ def visualise(ax, pose, color, label):
     if label:
         ax.text(x + 0.1, y + 0.1, label, color=color)
 
-agent_pose = [1, 1, np.pi / 8]  # Pose X
-helper_pose = [2, 1, np.pi / 4]  # Pose Y
-agent_pose_helper = get_pose(agent_pose, helper_pose)
+# agent_pose = [1, 1, np.pi / 2]
+# helper_pose = [2, 1, np.pi / 4]
+# agent_relative_pose = get_pose(agent_pose, helper_pose)
+# print(agent_relative_pose)
+#
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+# ax1.set_title("Environment's Perspective")
+# ax1.set_aspect('equal')
+# ax1.set_xlim(0, 5)
+# ax1.set_ylim(0, 5)
+# visualise(ax1, agent_pose, 'blue', 'Learning Agent')
+# visualise(ax1, helper_pose, 'red', 'Other Agent')
+# ax2.set_title("Other Agent's Perspective")
+# ax2.set_aspect('equal')
+# ax2.set_xlim(-2, 2)
+# ax2.set_ylim(-2, 2)
+# visualise(ax2, [0, 0, 0], 'red', 'Other Agent (Origin)')
+# visualise(ax2, agent_relative_pose, 'blue', 'Learning Agent')
+# plt.show()
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-ax1.set_title('Agent Perspective')
-ax1.set_aspect('equal')
-ax1.set_xlim(0, 5)
-ax1.set_ylim(0, 5)
-visualise(ax1, agent_pose, 'blue', 'Pose X')
-visualise(ax1, helper_pose, 'green', 'Pose Y')
-ax2.set_title("Helper Perspective")
-ax2.set_aspect('equal')
-ax2.set_xlim(-2, 2)
-ax2.set_ylim(-2, 2)
-visualise(ax2, [0, 0, 0], 'green', 'Pose Y (Origin)')
-visualise(ax2, agent_pose_helper, 'blue', 'Transformed Pose X')
-plt.show()
+
+
