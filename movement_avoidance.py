b072e95c71913acc327503509394b56838ff3f6c
Nitesh-777 <Nitesh.Karavadra@outlook.com>
2024-03-05 17:16:32 +0000

    attract, repulse & resultant methods created and tests run: - attract: takes global agent pos & goal pos, converts goal pos to relative pos in agents perspective and returns the angle between them. - repulse: takes global agent pos, object pos & max distance. max distance refers to range within object agent is to be repulsed (pushed) away. The degree of magnitude to be pushed determined by how close the agent has come to the object. this is reflected in magnitude returned. The angle as with attract is done the same way. - resultant angle: takes attract angle, repulse angle & repulse magnitude. Combines the attract and repulse angle to get a middle angle for where the agent should travel (between both points to avoid object while still going towards goal). It uses the repulse magnitude to determine how much it should go toward the mid-point or only diverge a small amount depending on how close it is to the object.

---
+++ movement_avoidance.py
@@ -0,0 +1,69 @@
+import numpy as np
+from transform_pose import get_pose_qpos
+
+
+def attract(agent_qpos, goal_qpos):
+    relative_goal_pos = get_pose_qpos(goal_qpos, agent_qpos)
+    goal_rel_coords = relative_goal_pos[:2]
+
+    attract_angle = np.arctan2(goal_rel_coords[1], goal_rel_coords[0])
+    attract_angle = attract_angle % (2 * np.pi)
+
+    return attract_angle
+
+
+def repulse_angle(agent_qpos, object_qpos, max_dist, intensity=1.0):
+    relative_food_pos = get_pose_qpos(object_qpos, agent_qpos)
+    object_rel_coords = relative_food_pos[:2]
+    # print(relative_food_pos)
+    angle = np.arctan2(object_rel_coords[1], object_rel_coords[0])
+    if angle < 0:
+        angle += 2 * np.pi
+    repulse_angle = angle + np.pi
+    repulse_angle = repulse_angle % (2 * np.pi)
+
+    object_dist = np.linalg.norm(object_rel_coords)
+    repulse_magnitude = (max_dist - (object_dist * intensity)) / max_dist
+    if repulse_magnitude < 0:
+        repulse_magnitude = 0
+
+    return repulse_angle, repulse_magnitude
+
+
+def resultant_angle(attract_angle, repulse_angle, repulse_magnitude, max_repulse_magnitude=1.0):
+    repulse_magnitude = repulse_magnitude / max_repulse_magnitude
+    delta_angle = np.arctan2(np.sin(repulse_angle - attract_angle), np.cos(repulse_angle - attract_angle))
+
+    resultant_angle = attract_angle + (delta_angle * repulse_magnitude)
+    resultant_angle = resultant_angle % (2 * np.pi)
+
+    return resultant_angle
+
+
+agent_test_qpos = [1, 1, 0.06999969, 0.38268343, 0, 0, 0.92387953]
+food_test_pos = [2.00000000e+00, 2.00000000e+00, 3.09632818e-01, 1.00000000e+00, 1.10641257e-18, 0.00000000e+00, 0.00000000e+00]
+goal_test_pos = [3.00000000e+00, 3.00000000e+00, 3.09632818e-01, 1.00000000e+00, 1.10641257e-18, 0.00000000e+00, 0.00000000e+00]
+repulse_ang, repulse_mag = repulse_angle(agent_test_qpos, food_test_pos, 2.828427)
+repulse_ang_d = repulse_ang * 180 / np.pi
+print(repulse_ang_d)
+print(repulse_mag)
+print('----------------------')
+attract_ang = attract(agent_test_qpos, goal_test_pos)
+attract_ang_d = attract_ang * 180 / np.pi
+print(attract_ang_d)
+print("@@@@@@@@@@@@@@@@@@@@@@")
+resultant_ang2 = resultant_angle(attract_ang, repulse_ang, repulse_mag)
+print(resultant_ang2)
+resultant_ang2_d = resultant_ang2 * 180 / np.pi
+print(resultant_ang2_d)
+
+
+
+
+
+
+
+
+
+
+
