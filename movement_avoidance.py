85a087df1317aee8f7bd31bd5a8cac3e2c3084db
Nitesh-777 <Nitesh.Karavadra@outlook.com>
2024-03-07 00:42:52 +0000

    angle_to_wheel method added which takes the current angle of the target position relative to its perspective and then normalise this angle to between -pi & pi. Then checking if this angle falls withing the given threshold (default = 10 degrees) in which case it moves forward and returns an action vector [1, 1]. In the other case it checks whether the angle falls towards the left (< 0) or towards the right (> 0) and returns the correlating vector.

--- movement_avoidance.py
+++ movement_avoidance.py
@@ -12,7 +12,7 @@ def attract(agent_qpos, goal_qpos):
     return attract_angle
 
 
-def repulse_angle(agent_qpos, object_qpos, max_dist, intensity=1.0):
+def repulse(agent_qpos, object_qpos, max_dist, intensity=1.0):
     relative_food_pos = get_pose_qpos(object_qpos, agent_qpos)
     object_rel_coords = relative_food_pos[:2]
     # print(relative_food_pos)
@@ -30,7 +30,7 @@ def repulse_angle(agent_qpos, object_qpos, max_dist, intensity=1.0):
     return repulse_angle, repulse_magnitude
 
 
-def resultant_angle(attract_angle, repulse_angle, repulse_magnitude, max_repulse_magnitude=1.0):
+def resultant(attract_angle, repulse_angle, repulse_magnitude, max_repulse_magnitude=1.0):
     repulse_magnitude = repulse_magnitude / max_repulse_magnitude
     delta_angle = np.arctan2(np.sin(repulse_angle - attract_angle), np.cos(repulse_angle - attract_angle))
 
@@ -39,23 +39,37 @@ def resultant_angle(attract_angle, repulse_angle, repulse_magnitude, max_repulse
 
     return resultant_angle
 
+def angle_to_wheel(target_angle, threshold=np.radians(10)):
 
-agent_test_qpos = [1, 1, 0.06999969, 0.38268343, 0, 0, 0.92387953]
-food_test_pos = [2.00000000e+00, 2.00000000e+00, 3.09632818e-01, 1.00000000e+00, 1.10641257e-18, 0.00000000e+00, 0.00000000e+00]
-goal_test_pos = [3.00000000e+00, 3.00000000e+00, 3.09632818e-01, 1.00000000e+00, 1.10641257e-18, 0.00000000e+00, 0.00000000e+00]
-repulse_ang, repulse_mag = repulse_angle(agent_test_qpos, food_test_pos, 2.828427)
-repulse_ang_d = repulse_ang * 180 / np.pi
-print(repulse_ang_d)
-print(repulse_mag)
-print('----------------------')
-attract_ang = attract(agent_test_qpos, goal_test_pos)
-attract_ang_d = attract_ang * 180 / np.pi
-print(attract_ang_d)
-print("@@@@@@@@@@@@@@@@@@@@@@")
-resultant_ang2 = resultant_angle(attract_ang, repulse_ang, repulse_mag)
-print(resultant_ang2)
-resultant_ang2_d = resultant_ang2 * 180 / np.pi
-print(resultant_ang2_d)
+    norm_angle = (target_angle + np.pi) % (2 * np.pi) - np.pi
+
+    if abs(norm_angle) < threshold:
+        return [1, 1]
+    else:
+        if norm_angle > 0:
+            # Rotating towards the left
+            return [-0.5, 0.5]
+        else:
+            # Rotating towards the right
+            return [0.5, -0.5]
+
+
+# agent_test_qpos = [1, 1, 0.06999969, 0.38268343, 0, 0, 0.92387953]
+# food_test_pos = [2.00000000e+00, 2.00000000e+00, 3.09632818e-01, 1.00000000e+00, 1.10641257e-18, 0.00000000e+00, 0.00000000e+00]
+# goal_test_pos = [3.00000000e+00, 3.00000000e+00, 3.09632818e-01, 1.00000000e+00, 1.10641257e-18, 0.00000000e+00, 0.00000000e+00]
+# repulse_ang, repulse_mag = repulse(agent_test_qpos, food_test_pos, 2.828427)
+# repulse_ang_d = repulse_ang * 180 / np.pi
+# print(repulse_ang_d)
+# print(repulse_mag)
+# print('----------------------')
+# attract_ang = attract(agent_test_qpos, goal_test_pos)
+# attract_ang_d = attract_ang * 180 / np.pi
+# print(attract_ang_d)
+# print("@@@@@@@@@@@@@@@@@@@@@@")
+# resultant_ang2 = resultant(attract_ang, repulse_ang, repulse_mag)
+# print(resultant_ang2)
+# resultant_ang2_d = resultant_ang2 * 180 / np.pi
+# print(resultant_ang2_d)
 
 
 
